d004d6dd02b45100b443fd500756ebef58c5b0dc
Yashioru <yashioruaccs@gmail.com>
2025-02-12 10:05:06 +0100

    lol

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -40,14 +40,14 @@ const Hero = () => {
   return (
     <div
       ref={heroRef}
-      className={`relative w-full h-screen bg-gray-800 flex items-center justify-center transition-opacity duration-1000 ${
+      className={`relative w-full h-screen bg-black flex items-center justify-center transition-opacity duration-1000 ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}>
       <div className="absolute inset-0 bg-black dark:bg-gray-900 opacity-20 z-10"></div>
       <div className="relative grid grid-cols-2 w-full h-full z-20">
-        <div className="bg-white dark:bg-gray-800 flex items-center justify-center font-bold text-white dark:text-white z-30">
+        <div className="bg-white dark:bg-white flex items-center justify-center font-bold text-black dark:text-black z-30">
           <div
-            className={`hover:cursor-pointer font-serif font-bold text-3xl sm:text-5xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-white dark:text-white transform transition-transform duration-1000 text-center ${
+            className={`hover:cursor-pointer font-serif font-bold text-3xl sm:text-5xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-black dark:text-black transform transition-transform duration-1000 text-center ${
               isVisible ? "translate-y-0" : "-translate-y-10"
             }`}
             onClick={() => scrollToSection("part1")}>
